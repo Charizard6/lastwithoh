b6c3beffa9840a9cb73f373842a4423726877ee5
육영현 <yyh6066@email.daelim.ac.kr>
2022-12-08 14:11:04 +0900

    firstClick

--- js/example.js
+++ js/example.js
@@ -1,3 +1,7 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable eol-last */
-alert('dd');
+const nameH1Element = document.querySelector('h1.inline');
+// eslint-disable-next-line linebreak-style
+nameH1Element.onClick = () => {
+  // eslint-disable-next-line linebreak-style
+  alert('클릭되었습니다');
+// eslint-disable-next-line linebreak-style
+};
