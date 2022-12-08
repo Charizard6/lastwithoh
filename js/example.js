36f53e73629b44dabd2984215e2cb391ba04ce55
육영현 <yyh6066@email.daelim.ac.kr>
2022-12-08 14:28:34 +0900

    as

--- js/example.js
+++ js/example.js
@@ -1,11 +1,22 @@
 /* eslint-disable linebreak-style */
 const nameH1Element = document.querySelector('h1.inline');
+const connectNameElement = document.querySelector('span.red');
+
+
 const localName = localStorage.getItem('name');
-if (localName) nameH1Element.textContent = localName;
+if (localName) {
+  nameH1Element.textContent = localName;
+  connectNameElement.textContent = localName;
+}
 
 
 nameH1Element.onClick = () => {
   const inputName = prompt('이름을 입력해주세요.');
-  localStorage.setItem('name', inputName);
-  nameH1Element.textContent = inputName;
+  if (inputName) {
+    localStorage.setItem('name', inputName);
+    nameH1Element.textContent = inputName;
+    connectNameElement.textContent = inputName;
+  } else {
+    alert('이름이 안입력됨');
+  }
 };
