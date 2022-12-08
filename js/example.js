c9ff26f3e740b616344bb93437d9fb8692b985c5
육영현 <yyh6066@email.daelim.ac.kr>
2022-12-08 14:21:46 +0900

    ds

--- js/example.js
+++ js/example.js
@@ -1,6 +1,10 @@
 /* eslint-disable linebreak-style */
 const nameH1Element = document.querySelector('h1.inline');
+const localName = localStorage.getItem('name');
+nameH1Element.textContent = localName;
+
 nameH1Element.onClick = () => {
   const inputName = prompt('이름을 입력해주세요.');
+  localStorage.setItem('name', inputName);
   nameH1Element.textContent = inputName;
 };
