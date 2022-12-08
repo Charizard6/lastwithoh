179f0c883a0d7c149eaa5556f62b1f09000413b3
육영현 <yyh6066@email.daelim.ac.kr>
2022-12-08 14:59:24 +0900

    modal1

--- js/example.js
+++ js/example.js
@@ -2,6 +2,8 @@
 const nameH1Element = document.querySelector('h1.inline');
 const connectNameElement = document.querySelector('span.red');
 
+const inputModalElement = document.querySelector('#inputModal');
+
 const setUserName = (name) => {
   nameH1Element.textContent = name;
   connectNameElement.textContent = name;
@@ -12,11 +14,15 @@ if (localName) setUserName(localName);
 
 
 nameH1Element.onclick = () => {
-  const inputName = prompt('이름을 입력해주세요.');
+  inputModalElement.showModal();
+};
+/* const inputName = prompt('이름을 입력해주세요.');
   if (inputName) {
     localStorage.setItem('name', inputName);
     setUserName(inputName);
   } else {
     alert('이름이 안입력됨');
-  }
+  }*/
+inputModalElement.onclick = (event) => {
+  if (event.target.nodeNAme === 'DIALOG') inputModalElement.close();
 };
