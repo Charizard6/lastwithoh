7c94c5cfdc867d42a465d10e8740f73c21029f38
육영현 <yyh6066@email.daelim.ac.kr>
2022-12-08 14:14:30 +0900

    dd

--- js/example.js
+++ js/example.js
@@ -1,7 +1,6 @@
+/* eslint-disable linebreak-style */
 const nameH1Element = document.querySelector('h1.inline');
-// eslint-disable-next-line linebreak-style
 nameH1Element.onClick = () => {
-  // eslint-disable-next-line linebreak-style
-  alert('클릭되었습니다');
-// eslint-disable-next-line linebreak-style
+  const inputName = prompt('이름을 입력해주세요.');
+  nameH1Element.textContent = inputName;
 };
