68b90634b0e7787e02652bdce312903a70b68348
육영현 <yyh6066@email.daelim.ac.kr>
2022-12-08 15:37:25 +0900

    gwaje1

--- js/example.js
+++ js/example.js
@@ -3,10 +3,20 @@ const nameH1Element = document.querySelector('h1.inline');
 const connectNameElement = document.querySelector('span.red');
 
 const inputModalElement = document.querySelector('#inputModal');
+const modalSubmitButton = document.querySelector('button.modalSubmit');
+
+const connectNo = document.querySelector('.studentNo');
+const connectEmail = document.querySelector('.email');
 
 const setUserName = (name) => {
   nameH1Element.textContent = name;
   connectNameElement.textContent = name;
+};
+const setUserNo = (sNo) => {
+  connectNo.textContent = sNo;
+};
+const setUserEmail = (eemail) => {
+  connectEmail.textContent = eemail;
 };
 
 const localName = localStorage.getItem('name');
@@ -24,6 +34,19 @@ nameH1Element.onclick = () => {
     alert('이름이 안입력됨');
   }*/
 inputModalElement.onclick = (event) => {
-  if (event.target.nodeNAme === 'DIALOG') inputModalElement.close();
-  console.log(event.target);
+  if (event.target.nodeName === 'DIALOG') inputModalElement.close();
+  console.log(event.target.nodeName);
+};
+
+modalSubmitButton.onclick = () =>{
+  const modalFormElement = document.querySelector('.modalForm');
+  const formData = new FormData(modalFormElement);
+
+  for (const [key, value] of formData) {
+    localStorage.setItem(key, value);
+    if (key === 'userName') setUserName(value);
+    if (key === 'studentNo') setUserNo(value);
+    if (key === 'email') setUserEmail(value);
+  }
+  inputModalElement.close();
 };
